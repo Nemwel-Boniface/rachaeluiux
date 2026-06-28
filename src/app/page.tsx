0634ce521037572c8299822f9f1d21a9da593f65
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-06-28 17:18:16 +0300

    feat: add Footer to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Landing from "@/components/sections/landing";
 import FeaturedProjects from "@/components/sections/featured-projects";
 import AreasOfFocus from "@/components/sections/areas-of-focus";
 import DesigningWithPurpose from "@/components/sections/design-with-purpose";
+import Footer from "@/components/sections/footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <FeaturedProjects />
       <AreasOfFocus />
       <DesigningWithPurpose />
+      <Footer />
     </main>
   );
 }
